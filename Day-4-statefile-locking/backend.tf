eef3085ebe08eb42909cc8311c29bb2fabf40883
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-10-30 18:02:57 +0530

    modify Day-4

--- Day-4-statefile-locking/backend.tf
+++ Day-4-statefile-locking/backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket = "my-statefile-storage"
     key    = "terraform.tfstate"
     #use_lockfile = true # to use s3 native locking 1.19 version above
-    use_lockfile = true
     region = "us-east-1"
+     dynamodb_table = "veera" # any version we can use dynamodb locking 
+    encrypt = true
   }
 }
