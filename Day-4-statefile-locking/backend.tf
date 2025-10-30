e4ec66dccac2f4497fd46da3ce479572b5eb8fe3
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-10-30 17:39:52 +0530

    update Day-4

--- Day-4-statefile-locking/backend.tf
+++ Day-4-statefile-locking/backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "my-statefile-storage"
     key    = "terraform.tfstate"
     #use_lockfile = true # to use s3 native locking 1.19 version above
+    use_lockfile = true
     region = "us-east-1"
   }
 }
