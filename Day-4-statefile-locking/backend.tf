a7e1b0741419e94231889e7b91dc50997ff8c4e9
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-10-30 18:04:51 +0530

    new

--- Day-4-statefile-locking/backend.tf
+++ Day-4-statefile-locking/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "terraform.tfstate"
     #use_lockfile = true # to use s3 native locking 1.19 version above
     region = "us-east-1"
-     dynamodb_table = "veera" # any version we can use dynamodb locking 
+     dynamodb_table = "SHANKAR" # any version we can use dynamodb locking 
     encrypt = true
   }
 }
