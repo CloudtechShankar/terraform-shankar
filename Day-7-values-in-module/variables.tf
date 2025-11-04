5dca416dfe30bdb3a9991e61d7f7a4f794b0b213
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-11-04 21:59:46 +0530

    modifying day-6 and day-7

---
+++ Day-7-values-in-module/variables.tf
@@ -0,0 +1,46 @@
+variable "ami_id" {
+    description = "passing ami values"
+    default = ""
+    type = string
+  
+}
+variable "type" {
+    description = "passing values to instance type"
+    default = ""
+    type = string
+  
+}
+
+
+}
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "availability_zones" {
+  type = list(string)
+}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "enable_nat_gateway" {
+  type = bool
+}
+
+variable "single_nat_gateway" {
+  type = bool
+}
+
+variable "tags" {
+  type = map(string)
+}
