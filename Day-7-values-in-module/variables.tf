2235298a1d9744f459f7584311992450b068f599
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-11-05 12:22:20 +0530

    Day-7-modules

--- Day-7-values-in-module/variables.tf
+++ Day-7-values-in-module/variables.tf
@@ -10,37 +10,3 @@ variable "type" {
     type = string
   
 }
-
-
-}
-variable "vpc_name" {
-  type = string
-}
-
-variable "vpc_cidr" {
-  type = string
-}
-
-variable "availability_zones" {
-  type = list(string)
-}
-
-variable "private_subnets" {
-  type = list(string)
-}
-
-variable "public_subnets" {
-  type = list(string)
-}
-
-variable "enable_nat_gateway" {
-  type = bool
-}
-
-variable "single_nat_gateway" {
-  type = bool
-}
-
-variable "tags" {
-  type = map(string)
-}
